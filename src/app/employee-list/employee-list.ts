9557fe9fee3df4eaf4748cf3a743ad630b37592e
Madhuri3199 <emadhuri2021@gmail.com>
2026-02-24 19:56:34 +0530

    Implement Add Employee feature with two-way binding

---
+++ src/app/employee-list/employee-list.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './employee-list.html',
+  styleUrl: './employee-list.css',
+})
+export class EmployeeList {
+  employees = [
+    { id: 1, name: 'John Doe', role: 'Frontend Developer' },
+    { id: 2, name: 'Jane Smith', role: 'Backend Developer' },
+    { id: 3, name: 'Michael Johnson', role: 'UI Designer' },
+  ];
+
+  newEmployee = {
+    id: 0,
+    name: '',
+    role: '',
+  };
+
+  addEmployee() {
+    if (this.newEmployee.name && this.newEmployee.role) {
+      const newId = this.employees.length + 1;
+
+      this.employees.push({
+        id: newId,
+        name: this.newEmployee.name,
+        role: this.newEmployee.role,
+      });
+
+      this.newEmployee = { id: 0, name: '', role: '' };
+    }
+  }
+}
